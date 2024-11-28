d168fc56ccee5d29ce191de4028021bda4935345
Melisa Melayim <melisamelayim@hotmail.com>
2024-11-28 18:10:18 +0300

    Initial Commit

---
+++ Moviepedia/MoviepediaApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoviepediaApp.swift
+//  Moviepedia
+//
+//  Created by Max on 28.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct MoviepediaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
